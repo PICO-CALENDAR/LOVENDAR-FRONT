00f3d35d250678bc3e4181d45a408d7daa559a05
GyuriKim <artgr2062@konkuk.ac.kr>
2025-02-25 01:14:00 +0900

    feat: 타임캡슐 조회 페이지 구현중

---
+++ lib/memories/view/timecapsule_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class TimecapsuleScreen extends StatelessWidget {
+  const TimecapsuleScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
